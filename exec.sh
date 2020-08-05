4953b1a2b948b4a7b93c09e4ddd5e8360c97d5a2
Luis Redda <luis.redda@gmail.com>
2020-08-05 17:36:45 -0300

    Create exec.sh

---
+++ exec.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+echo "This message coming from your new Drone plugin!"
+echo ${PLUGIN_APPLICATION}
+echo ${PLUGIN_PIPELINE}
+echo ${PLUGIN_SERVICE}
+echo ${PLUGIN_BUILD}
+echo ${PLUGIN_ACCOUNTID}
+echo ${PLUGIN_APIKEY}
+echo ${PLUGIN_BODY}
